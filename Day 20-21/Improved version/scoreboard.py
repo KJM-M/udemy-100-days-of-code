0037f579148534f0aaeda5c632d2d7083da260a4
KJM-M <jesse.mustajarvi@gmail.com>
2026-03-21 23:50:12 +0200

    Day 23 / Intermediate / Add a High Score to the Snake Game

---
+++ Day 20-21/Improved version/scoreboard.py
@@ -0,0 +1,32 @@
+from turtle import Turtle
+
+ALIGNMENT = "center"
+FONT = ("Courier", 16, "normal")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        with open("./high_score.txt", mode="r") as f:
+            self.high_score = int(f.read())
+        self.color("white")
+        self.penup()
+        self.hideturtle()
+        self.goto(0, 270)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"score: {self.score} High score: {self.high_score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.update_scoreboard()
+
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("./high_score.txt", mode="w") as f:
+                f.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
