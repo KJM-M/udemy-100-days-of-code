4fe9db731074a9048a7bdb239c44953cd2f60a70
KJM-M <jesse.mustajarvi@gmail.com>
2026-04-08 20:01:24 +0300

    Day 26 / Intermediate / List Comprehension and the NATO Alphabet - Improved

--- Day 26/main.py
+++ Day 26/main.py
@@ -72,6 +72,16 @@ df = pandas.read_csv("./nato_phonetic_alphabet.csv")
 df_dict = {row.letter:row.code for (index, row) in df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-user_input = input("Enter your name: ").upper()
-result = [df_dict[letters] for letters in user_input if letters in df_dict]
-print(result)
+
+
+def generate_phonetic():
+    user_input = input("Enter your name: ").upper()
+    try:
+        result = [df_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(result)
+
+generate_phonetic()
